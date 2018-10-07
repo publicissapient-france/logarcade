7af79377f7f2aa33199c782134e2e28a4ea37754
Julien Smadja <julien.smadja@gmail.com>
2018-10-07 15:44:23 +0200

    Controls P2 Keyboard

--- src/controls.js
+++ src/controls.js
@@ -5,6 +5,16 @@ const controlsPlayer1_Keyboard = {
     D: 'T',
     START: 'SPACE',
 };
+
+const controlsPlayer2_Keyboard = {
+    A: 'U',
+    B: 'I',
+    C: 'O',
+    D: 'P',
+    START: 'ENTER',
+};
+
 const CONTROLS_P1 = controlsPlayer1_Keyboard;
+const CONTROLS_P2 = controlsPlayer2_Keyboard;
 
 module.exports = {CONTROLS_P1};
