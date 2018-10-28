539b237db2f13567630d83c84cc7f078ce452c6a
paque <paque.melanie@gmail.com>
2018-10-28 12:25:44 +0100

    add background

--- src/scenes/game-two-player.js
+++ src/scenes/game-two-player.js
@@ -1,4 +1,6 @@
 const Screen = require('../screen');
+const {INITIAL_REMAINING_TIME} = require('../game');
+
 const { HealthBar } = require('../healthbar');
 
 class SceneGameTwoPlayers extends Phaser.Scene {
@@ -7,10 +9,20 @@ class SceneGameTwoPlayers extends Phaser.Scene {
     }
 
     preload() {
+        this.load.image('bg', 'assets/backgrounds/BG.png');
         this.add.text(Screen.WIDTH / 2, Screen.HEIGHT / 2, 'sceneGameTwoPlayers', {font:"bold 24px VT323"});
     }
 
     create() {
+        this.bg = this.add.image(0, 0, 'bg').setOrigin(0);
+        this.bg.setScale(Screen.ZOOM, Screen.ZOOM);
+        this.bg.setZ(-1);
+
+
+        this.remainingTime = INITIAL_REMAINING_TIME;
+        this.time = this.add.text(Screen.WIDTH / 2, 50, this.remainingTime, {font:"bold 24px VT323"});
+        this.start = new Date();
+
 
         this.healthbarPlayerOne = new HealthBar(this, { x: 30, y: 30, bar: {color : 0xFF6347, direction: -1 }});
         console.log("healthbarPlayerOne",this.healthbarPlayerOne);
@@ -20,6 +32,12 @@ class SceneGameTwoPlayers extends Phaser.Scene {
         console.log("healthbarPlayerTwo",this.healthbarPlayerTwo);
 
         this.healthbarPlayerTwo.setProgress(100);
+    }
+
+    update(){
+        const elapsedTime = (new Date().getTime() - this.start.getTime()) / 1000;
+        this.remainingTime = Math.round(INITIAL_REMAINING_TIME - elapsedTime);
+        this.time.setText(this.remainingTime);
     }
 }
 
