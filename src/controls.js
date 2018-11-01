f7f6195783766fd7ee968f20a653d1b9ac9d6e90
Julien Smadja <julien.smadja@gmail.com>
2018-11-01 18:19:29 +0100

    JPAC Controls

--- src/controls.js
+++ src/controls.js
@@ -18,7 +18,33 @@ const controlsPlayer2_Keyboard = {
     START: 'ENTER',
 };
 
-const CONTROLS_P1 = controlsPlayer1_Keyboard;
-const CONTROLS_P2 = controlsPlayer2_Keyboard;
+const controlsPlayer1_Jpac = {
+    A: 'CTRL',
+    B: 'ALT',
+    C: 'SPACE',
+    D: 'SHIFT',
+    START: 'ONE',
+    UP: 'UP',
+    DOWN: 'DOWN',
+    LEFT: 'LEFT',
+    RIGHT: 'RIGHT',
+};
+
+const controlsPlayer2_Jpac = {
+    A: 'A',
+    B: 'S',
+    C: 'Q',
+    D: 'W',
+    START: 'TWO',
+    UP: 'R',
+    DOWN: 'F',
+    LEFT: 'D',
+    RIGHT: 'G',
+};
+
+const JPAC = false;
+
+const CONTROLS_P1 = JPAC ? controlsPlayer1_Jpac : controlsPlayer1_Keyboard;
+const CONTROLS_P2 = JPAC ? controlsPlayer2_Jpac : controlsPlayer2_Keyboard;
 
 module.exports = {CONTROLS_P1, CONTROLS_P2};
